0ea461a5b26bf0b9f3ad47cae4cb94be7d72d225
Oliver Borg <ojab2@cam.ac.uk>
2026-01-31 17:03:29 +0000

    Extended reconstruction script

--- reconstruct.sh
+++ reconstruct.sh
@@ -1,19 +1,28 @@
 export CUDA_VISIBLE_DEVICES=2
 
-choices=("colmap" "vggt")
-nums=(1 2 4 8 10 20 30 40 50 75 100 125 150 175 200)
+choices=("vggt") # "colmap" 
+# nums=(1 2 4 8 10 20 30 40 50 75 100 125 150 175 200)
+nums=(10 20 50 100)
+# nums=(20)
 dataset="bonsai"
-seeds=(43 44)
+seeds=(42 43 44)
+factor=2
+conf_thres_values=(2.0 3.0 4.0 5.0)
+# conf_thres_values=(1.0 1.5 2.0)
 
 for choice in "${choices[@]}"; do
     for num in "${nums[@]}"; do
         for seed in "${seeds[@]}"; do
-            python -m reconstruct \
-                --input ./data/360_v2/$dataset/images_8 \
-                --name "${dataset}_8" \
-                --choice $choice \
-                --num_images $num \
-                --seed $seed
+            for conf_thres_value in "${conf_thres_values[@]}"; do
+                python -m reconstruct \
+                    --input ./data/360_v2/$dataset/images_${factor} \
+                    --name "${dataset}_${factor}" \
+                    --choice $choice \
+                    --num_images $num \
+                    --seed $seed \
+                    --conf_thres_value $conf_thres_value
+                python -m check_sparse ./${choice}_outputs/${dataset}_${factor}_n${num}_s${seed}_c${conf_thres_value}/sparse
+            done
         done
     done
 done
